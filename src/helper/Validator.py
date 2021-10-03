400d49ab142e75c0bfb22e3e507d7c14415db3ea
hshima <shimada.henrique@gmail.com>
2021-10-03 17:51:08 -0300

    Aplicação de tipos nos parametros

--- src/helper/Validator.py
+++ src/helper/Validator.py
@@ -80,13 +80,14 @@ class Validator(object):
         'param_3': 'value_3'
     }
 
-    def __init__(self, event):
+    def __init__(self, event: dict):
         self.path = event['path']
         self.method = event['httpMethod']
         self.single_param = event['queryStringParameters']
         self.array_string = event['multiValueQueryStringParameters']
+        self.queriable = {'path': self.path}
 
-    def __get_path__(self):
+    def __get_path__(self) -> str:
         logger.info(msg='get_path')
         # return next((path for path in self.__allowedPaths__ if path == self.path), 'ValueError')
         return self.__allowedPaths__.get(self.path, 'ValueError')
@@ -95,17 +96,17 @@ class Validator(object):
         logger.info(msg='check_null_params')
         return self.single_param is None or self.array_string is None
 
-    def __allows_null_param_path__(self, path) -> bool:
+    def __allows_null_param_path__(self, path: str) -> bool:
         logger.info(msg='check_nullable_param_path')
         allows = self.__allowedPaths__.get(path)['nullable_param']
         null = self.__is_null_parameters__()
         return True if allows else not null
 
-    def __is_allowed_http_method__(self, method, path):
+    def __is_allowed_http_method__(self, method: str, path: str) -> bool:
         logger.info(msg='check_http_method_validity')
         return True if method.upper() in self.__allowedPaths__[path]['allowedMethods'] else False
 
-    def check_parameters(self):
+    def check_parameters(self) -> (str, dict):
         if self.__get_path__() == 'ValueError':
             raise Exception(MESSAGE.INVALID_ENDPOINT.value)
         if not self.__is_allowed_http_method__(self.method, self.path):
